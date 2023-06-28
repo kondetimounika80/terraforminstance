9408e62d9dee806c27ec2a7a05c6aac83abc8d95
Akhil2598 <98694420+Akhil2598@users.noreply.github.com>
2023-06-28 11:05:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "instancecee" {
+resource "aws_instance" "akhil-ec2instance" {
   ami           = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.micro"
-  key_name = "pemkey"
+  instance_type = "t2.medium"
+  key_name = "akhil"
 }
