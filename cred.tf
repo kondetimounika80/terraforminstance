76cd76f4c9c35fdf401317078b23f6a4b1aef37a
kondetimounika80 <128279253+kondetimounika80@users.noreply.github.com>
2023-06-29 12:01:42 +0530

    Update cred.tf

--- cred.tf
+++ cred.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3FLFK4YXEAKQYHM3"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
